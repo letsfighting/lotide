6d4c7b82421d75e14bfb54b26612cbc572c816b4
Elroy Hui <huielroy@hotmail.com>
2022-03-08 16:54:24 -0500

    implemented eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,31 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✔✔✔ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (firstVal, secondVal) {
+  if (firstVal.length !== secondVal.length) {
+    return false;
+  }
+
+  for (let i = 0; i < firstVal.length; i++) {
+    if (firstVal[i] !== secondVal[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
