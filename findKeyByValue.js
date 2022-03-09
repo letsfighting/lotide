77c4f366c4598dcba9fcccece0d2058821928022
Elroy Hui <huielroy@hotmail.com>
2022-03-09 17:17:04 -0500

    implemented findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✔✔✔ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (showObject, queriedShow) {
+  for (const show in showObject) {
+    if (showObject[show] === queriedShow) {
+      return show;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
