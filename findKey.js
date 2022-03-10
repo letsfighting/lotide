d11b783ab19daeab0fa8fa0951f551b076c551b7
Elroy Hui <huielroy@hotmail.com>
2022-03-10 18:51:09 -0500

    implemented findKey.js

---
+++ findKey.js
@@ -0,0 +1,57 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✔✔✔ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function (list, callback) {
+  for (const property in list) {
+    if (callback(list[property])) {
+      return property;
+    }
+  }
+  return undefined;
+};
+
+let result1 = findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 2
+); // => "noma"
+
+let result2 = findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 3
+);
+
+let result3 = findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 4
+);
+
+assertEqual(result1, "noma");
+assertEqual(result2, "Akaleri");
+assertEqual(result2, "Akaleri");
