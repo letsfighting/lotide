6564f5b508b5f024d839902eb092626f3cdc12ff
Elroy Hui <huielroy@hotmail.com>
2022-03-14 19:41:00 -0400

    implemented testing module export/require

--- assertEqual.js
+++ assertEqual.js
@@ -7,8 +7,5 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 3);
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
+
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,10 @@
+// test/assertEqualTest.js
+
+const assertEqual = require("../assertEqual");
+
+// TEST CODE
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 3);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
