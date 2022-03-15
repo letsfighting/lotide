e2ac1291ca8e8c041388256f519f955715f758b1
Elroy Hui <huielroy@hotmail.com>
2022-03-14 20:18:57 -0400

    implemented assertArraysEqualTest.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+const eqArrays = require("./eqArrays");
+
 const assertArraysEqual = function (actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`✔✔✔ Assertion Passed: ${actual} === ${expected}`);
@@ -6,20 +8,6 @@ const assertArraysEqual = function (actual, expected) {
   }
 };
 
-const eqArrays = function (firstVal, secondVal) {
-  if (firstVal.length !== secondVal.length) {
-    return false;
-  }
 
-  for (let i = 0; i < firstVal.length; i++) {
-    if (firstVal[i] !== secondVal[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+module.exports = assertArraysEqual;
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
