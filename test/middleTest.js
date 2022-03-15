9870409e7573bd815ef1c7162cedd8999d84fb63
Elroy Hui <huielroy@hotmail.com>
2022-03-14 22:22:26 -0400

    implemented chai based tests for middle and tail

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,5 @@
 // TEST/ASSERTION FUNCTIONS
-
+/*
 
 const assertArraysEqual = require("../assertArraysEqual");
 const middle = require("../middle");
@@ -10,3 +10,29 @@ assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
 assertArraysEqual(middle([1, 2]), []); // => []
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
 assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+
+*/
+
+const assert = require("chai").assert;
+const middle = require("../middle");
+
+describe('#middle', () => {
+  it('returns [] when [1] is passed', () => {
+    assert(middle([1]), []);
+  }) 
+  it('returns [2] when [1, 2, 3] is passed', () => {
+    assert(middle([1, 2, 3]), [2]);
+  }) 
+  it('returns [3] when [1, 2, 3, 4, 5] is passed', () => {
+    assert(middle([1, 2, 3, 4, 5]), [3]);
+  }) 
+  it('returns [] when [1] is passed', () => {
+    assert(middle([1, 2]), []);
+  }) 
+  it('returns [] when [1] is passed', () => {
+    assert(middle([1, 2, 3, 4]), [2, 3]);
+  }) 
+  it('returns [] when [1] is passed', () => {
+    assert(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  }) 
+})
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,5 @@
 // Test Case: Check the original array
-
+/*
 const tail = require("../tail.js");
 const assertEqual = require("../assertEqual.js")
 
@@ -7,4 +7,23 @@ const words = ["Yo Yo", "Lighthouse", "Labs"];
 console.log(tail(words)); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
 
+*/
+
+const tail = require('../tail');
+const assert = require('chai').assert;
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+
+
+describe('#tail', () => {
+  it('returns ["Lighthouse", "Labs"] when ["Yo Yo", "Lighthouse", "Labs"] is passed', () => {
+    assert.deepEqual(tail(words), [
+      "Lighthouse",
+      "Labs",
+    ]);
+  })
+  it('the original array remains unchanged', () => {
+    assert.strictEqual(words.length, 3);
+  })
+
+})
 
