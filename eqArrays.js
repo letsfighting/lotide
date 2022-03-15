38373b045a39aa8c33b80302f9f232f3fc7945ba
Elroy Hui <huielroy@hotmail.com>
2022-03-14 20:08:48 -0400

    implemented eqArraysTest.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✔✔✔ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const eqArrays = function (firstVal, secondVal) {
   if (firstVal.length !== secondVal.length) {
@@ -19,13 +12,6 @@ const eqArrays = function (firstVal, secondVal) {
   return true;
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
+module.exports = eqArrays;
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
